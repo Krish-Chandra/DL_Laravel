1fe745dd952c584b876d183e62c27ad23fdabb44
Krish-Chandra <krishchan@gmail.com>
2017-02-01 21:06:22 +0530

    Added more cols

--- resources/views/admin/request/index.blade.php
+++ resources/views/admin/request/index.blade.php
@@ -2,52 +2,52 @@
 
 @section('content')
 	<div>
+		<div class="panel panel-primary">
+			<div class="panel-heading">
+				Requests
+			</div>
 
-			@if (count($requests) > 0)
-				<div class="panel panel-primary">
-					<div class="panel-heading">
-						Requests
-					</div>
-
-					<div class="panel-body">
-						<table class="table table-bordered table-striped">
-							<thead>
-								<th>Book</th>
-								<th>Author</th>
-								<th>Total Copies</th>
-								<th>Available Copies</th>
-								<th>Requested By</th>
-								<th>Requested On</th>
-							</thead>
-							<tbody>
-								@for ($i = 0; $i < count($requests); $i++)
-									<tr>
-										<td>{{$requests[$i]['book']['title']}}</td>
-										<td>{{$requests[$i]['author']['authorname']}}</td>
-										<td>{{$requests[$i]['book']['total_copies']}}</td>
-										<td>{{$requests[$i]['book']['available_copies']}}</td>
-										<td>{{$requests[$i]['user']->name}}</td>
-										<td>{{$requests[$i]['request_date']}}</td>
-										<td>
-									        <form action="{{ url('admin/request/issuebook/'. $requests[$i]->id
-											)}}" method="POST">
-									            {!! csrf_field() !!}
-									            
-									            <button type="submit" title="Issue the book to the User?" class="btn btn-xs btn-link" id="issue-book-{{ $requests[$i]['id'] }}" >
-									                <i class="glyphicon glyphicon-ok" aria-hidden="true"></i>
-									            </button>
-									        </form>
-										</td>
-									</tr>
-								@endfor
-							</tbody>
-						</table>
-					</div>
-				</div>
+			<div class="panel-body">
+			@if (count($requests) > 0)			
+				<table class="table table-bordered table-striped">
+					<thead>
+						<th>Book</th>
+						<th>Author</th>
+						<th>Total Copies</th>
+						<th>Available Copies</th>
+						<th>Requested By</th>
+						<th>Requested On</th>
+					</thead>
+					<tbody>
+						@for ($i = 0; $i < count($requests); $i++)
+							<tr>
+								<td>{{$requests[$i]['book']['title']}}</td>
+								<td>{{$requests[$i]['author']['authorname']}}</td>
+								<td>{{$requests[$i]['book']['total_copies']}}</td>
+								<td>{{$requests[$i]['book']['available_copies']}}</td>
+								<td>{{$requests[$i]['user']->name}}</td>
+								<td>{{$requests[$i]['request_date']}}</td>
+								<td>
+							        <form action="{{ url('admin/request/issuebook/'. $requests[$i]->id
+									)}}" method="POST">
+							            {!! csrf_field() !!}
+							            
+							            <button type="submit" title="Issue the book to the User?" class="btn btn-xs btn-link" id="issue-book-{{ $requests[$i]['id'] }}" >
+							                <i class="glyphicon glyphicon-ok" aria-hidden="true"></i>
+							            </button>
+							        </form>
+								</td>
+							</tr>
+						@endfor
+					</tbody>
+				</table>
+			</div>
 			@else
 				<div class="alert alert-warning">
 					<span>No requests for books!</span>
 				</div>
 			@endif
+			
+		</div>
 	</div>
 @endsection
