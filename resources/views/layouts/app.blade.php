91fd77f20a0e69c87cdcac12abba5dd26d5c6e5b
Krish-Chandra <krishchan@gmail.com>
2017-02-17 21:18:04 +0530

    Title change

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>Laravel Quickstart - Basic</title>
+    <title>Digital Library</title>
 
     <link href="https://maxcdn.bootstrapcdn.com/bootswatch/3.3.6/simplex/bootstrap.min.css" rel="stylesheet" type="text/css"> 
     <!-- <link rel="stylesheet" type="text/css" href="{{asset('css/bootstrap.simplex.min.css')}}"/>     -->
